6f41592fe671afb5957fd0a97f79d59c4a1b29c5
rebeccasun <sun.ruoy@husky.neu.edu>
2020-05-09 17:59:17 -0700

    older browser support

--- public/App.js
+++ public/App.js
@@ -1,4 +1,12 @@
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+}); // pay attention to the quotes
+
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello ${c}!`); // pay attention to the quotes
+const message = helloContinents.join(' ');
+
 const element = (
     <div title="Outer div">
-        <h1>Hello World!</h1>
+        <h1>{message}</h1>
     </div>
 );
+
 ReactDOM.render(element, document.getElementById('contents'));
