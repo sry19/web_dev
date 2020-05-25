280a3ac30ab577b34bb8ccfa27ae16bce622d034
rebeccasun <sun.ruoy@husky.neu.edu>
2020-05-25 12:19:52 -0700

    graphql & about api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # web_dev
+
+Ch5:
+summary
+* What is GraphQL: https://www.zhihu.com/question/264629587
+* the nodemon tool that restarts the server on detecting changes to files by default only looks for changes to files with a .js extension. To make it watch for changes to other extensions, we need to add an -e option specifying all the extensions it needs to watch for. Since we added a file with extension .graphql, let’s specify js and graphql as the two extensions for this option.
+
+ issues and errors:
+* Initial setup of ApolloSever was repeatedly unsuccessful from pg. 97. After checking my work repeatedly, and running npm start, I kept receiving an error that read as follows: Error: Cannot find module 'graphql/validation/rules/PossibleTypeExtensions'. After cross referencing the author's repo I noted that his dependencies for apollo-server-express were set to version 2.3.1, whereas mine were only at 2.13.1. I ran npm install graphql@0 apollo-server-express@2.3 to force install the apollo server dependency to version 2.3+ instead, which solved the problem
--- server/server.js
+++ server/server.js
@@ -1,4 +1,32 @@
+const fs = require('fs') // use 'fs' module and readFaileSync function to read the file
 const express = require('express');//import the module and get the function that the module exports
+
+//About API
+const { ApolloServer } = require('apollo-server-express');
+let aboutMessage = "Issue Tracker API v1.0";
+
+
+const resolvers = {
+    Query: {
+      about: () => aboutMessage,
+    },
+    Mutation: {
+      setAboutMessage,
+    },
+  };
+
+  function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+  }
+  
+  /*
+  use the string that 'readFileSync' returned as the value for the property 'typeDefs' when creating the Apollo Server
+  */
+  const server = new ApolloServer({
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers,
+  });
+
 const app = express(); //instantiate an application
 /*
 If you wanted all static files to be accessed by a prefixed Url
@@ -6,6 +34,15 @@ a virtual path prefix (where the path does not actually exist in the file system
 https://expressjs.com/en/starter/static-files.html
 */
 app.use(express.static('public'));//generate a middleware and mount it on the application
+/*
+In order to use the same middleware for only requests matching a certain URL path, say, /public, 
+the app.use() method would have to be called with two arguments, the first one being the path, like this:
+    
+app.use('/public', express.static('public'));
+*/
+
+server.applyMiddleware({ app, path: '/graphql' });
+
 app.listen(3000, function() { // start the server and wait eternally for requests
     console.log('App started on port 3000'); // Ctrl+C to stop the server.
     /*
