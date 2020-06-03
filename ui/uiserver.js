81fe6d70d11dfd1a67ec8c4ffc81d2e006499266
rebeccasun <sun.ruoy@husky.neu.edu>
2020-06-02 17:44:38 -0700

    simple routing

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -7,6 +7,27 @@ if (apiProxyTarget) {
   app.use('/graphql', proxy({ target: apiProxyTarget }));
 }
 const app = express(); // instantiate an application
+
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+
+if (enableHMR && (process.env.NODE_ENV !== 'production')) {
+  console.log('Adding dev middleware, enabling HMR');
+  /* eslint "global-require": "off" */
+  /* eslint "import/no-extraneous-dependencies": "off" */
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  const config = require('./webpack.config.js');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 /*
 If you wanted all static files to be accessed by a prefixed Url
 a virtual path prefix (where the path does not actually exist in the file system)
