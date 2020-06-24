b4cb4f30d16b554989c59e39a0d17b2251c58cba
rebeccasun <sun.ruoy@husky.neu.edu>
2020-06-23 20:08:13 -0700

    issue counts api

--- api/api_handler.js
+++ api/api_handler.js
@@ -16,6 +16,7 @@ const resolvers = {
       about: about.getMessage,
       issueList: issue.list,
       issue: issue.get,
+      issueCounts: issue.counts,
     },
     Mutation: {
       setAboutMessage: about.setMessage,
