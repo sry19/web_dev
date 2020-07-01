af23dbf7d51e6a6faf2dbb3b16e2966a1ec1e1b8
rebeccasun <sun.ruoy@husky.neu.edu>
2020-06-30 18:56:05 -0700

    database url

--- api/scripts/trymongo.js
+++ api/scripts/trymongo.js
@@ -20,7 +20,7 @@ const { MongoClient } = require('mongodb');
 // const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 // Atlas URL - replace UUU with user, PPP with password, XXX with hostname
 //const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
-const url = 'mongodb+srv://new_user:19961116@cluster0-tkogw.mongodb.net/issuetracker';
+const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 // mLab URL - replace UUU with user, PPP with password, XXX with hostname
 // const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
 
